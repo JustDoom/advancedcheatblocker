64de14f528cf9872a7a223c1fb67a7694026ff5f
t0206 <70625710+t0206@users.noreply.github.com>
2021-07-18 04:17:49 +0900

    Delete src/main directory

--- src/main/java/com/advancedcheatblocker/acb/pluginanticheat/checks/CheckNames.java
+++
@@ -1,5 +0,0 @@
-package com.advancedcheatblocker.acb.pluginanticheat.checks;
-
-public enum CheckNames {
-    FlyA,FlyB,SpeedA,SpeedB,KillAura,Jesus,NoSlowDown,FlyC,FlyD,NoFallA,NoFallB,StepA,Timer,ScaffoldA,FlyE
-}
